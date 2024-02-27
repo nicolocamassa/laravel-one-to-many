b5a11bfd5625b14547160cde017e99ecb30d29c7
nicolocamassa <camassanicolo03@gmail.com>
2024-02-27 15:48:32 +0100

    [UPDATE] migrations for foreign key created

---
+++ database/migrations/2024_02_27_142814_projects_population.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->char('title', 30);
+            $table->text('description');
+            $table->char('image', 255);
+            $table->date('date');
+            $table->char('languages', 40);
+            $table->char('status', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
